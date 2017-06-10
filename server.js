6eb638f89f1b5482a86856aa98b64bc670e70ab3
wang <JingWang@JingWangs-MacBook-Pro.local>
2017-06-09 17:39:17 -0700

    Assignment2

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ var express = require('express');
 //initialize app as an express application
 var app = express();
 
-//the ip address for local server is always '127.0.0.1'.
-var ipaddress = '127.0.0.1';
-var port      = process.env.PORT || 3000;
-
-//go to folder public to find html files.
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname+'/public/assignment'));
 
-app.listen(port, ipaddress);
-
-console.log("hello world!");
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
